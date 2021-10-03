db3fae266a4e51430d4dfa0c63de9df962c16498
aroquega <aroquega@unsa.edu.pe>
2021-10-03 14:34:09 -0500

    Arregla los ids de los archivos de interfaces y agrega viewModel

--- app/src/main/java/com/lab03/visitadoresmedicos/MainActivity.java
+++ app/src/main/java/com/lab03/visitadoresmedicos/MainActivity.java
@@ -1,17 +1,31 @@
 package com.lab03.visitadoresmedicos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
+import com.lab03.visitadoresmedicos.databinding.ActivityMainBinding;
+import com.lab03.visitadoresmedicos.models.PatientViewModel;
+
 public class MainActivity extends AppCompatActivity {
+    private PatientViewModel viewModel;
+    private final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        viewModel = new ViewModelProvider(this).get(PatientViewModel.class);
+
+        binding.setLifecycleOwner(this);
+        binding.setActivity(this);
+        binding.setViewModel(viewModel);
 
         Button goToPatientButton = findViewById(R.id.go_to_patient_form_button);
         goToPatientButton.setOnClickListener((view) -> goToPatientRegistrationForm());
@@ -19,13 +33,17 @@ public class MainActivity extends AppCompatActivity {
         goToVisitFormButton.setOnClickListener((view) -> goToVisitForm());
     }
 
-    private void goToPatientRegistrationForm() {
+    public void goToPatientRegistrationForm() {
         Intent intent = new Intent(this, PatientFormActivity.class);
         startActivity(intent);
     }
 
-    private void goToVisitForm() {
+    public void goToVisitForm() {
         Intent intent = new Intent(this, VisitForm.class);
         startActivity(intent);
+    }
+
+    public void sendEmail(){
+        Log.d(TAG, "Sending email");
     }
 }
---
+++ app/src/main/java/com/lab03/visitadoresmedicos/models/BindingAdapters.java
@@ -0,0 +1,12 @@
+package com.lab03.visitadoresmedicos.models;
+
+import android.view.View;
+
+import androidx.databinding.BindingAdapter;
+
+public class BindingAdapters {
+    @BindingAdapter("app:visibility")
+    public static void showEmptyState(View v, boolean show) {
+        v.setVisibility(show ? View.VISIBLE : View.GONE);
+    }
+}
