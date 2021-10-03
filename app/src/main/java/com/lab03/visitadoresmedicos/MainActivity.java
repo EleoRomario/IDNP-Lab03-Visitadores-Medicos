5c20209df0270aa98475c01c43068dff3545071b
aroquega <aroquega@unsa.edu.pe>
2021-10-03 12:10:40 -0500

    añade las interfaces de usuario

--- app/src/main/java/com/lab03/visitadoresmedicos/MainActivity.java
+++ app/src/main/java/com/lab03/visitadoresmedicos/MainActivity.java
@@ -2,7 +2,9 @@ package com.lab03.visitadoresmedicos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button goToPatientButton = findViewById(R.id.go_to_patient_form_button);
+        goToPatientButton.setOnClickListener((view) -> goToPatientRegistrationForm());
+        Button goToVisitFormButton = findViewById(R.id.go_to_visit_form_button);
+        goToVisitFormButton.setOnClickListener((view) -> goToVisitForm());
+    }
+
+    private void goToPatientRegistrationForm() {
+        Intent intent = new Intent(this, PatientFormActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToVisitForm() {
+        Intent intent = new Intent(this, VisitForm.class);
+        startActivity(intent);
     }
 }
